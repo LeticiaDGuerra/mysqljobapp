54758bbb0a897253b9675c6fc03add6b81494404
Leticia Guerra <g.leticia@rocketmail.com>
2020-02-15 17:22:52 -0500

    update

---
+++ departments.sql
@@ -0,0 +1,47 @@
+-- * **department**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **name** - VARCHAR(30) to hold department name
+
+-- * **role**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **title** -  VARCHAR(30) to hold role title
+--   * **salary** -  DECIMAL to hold role salary
+--   * **department_id** -  INT to hold reference to department role belongs to
+
+-- * **employee**:
+
+--   * **id** - INT PRIMARY KEY
+--   * **first_name** - VARCHAR(30) to hold employee first name
+--   * **last_name** - VARCHAR(30) to hold employee last name
+--   * **role_id** - INT to hold reference to role employee has
+--   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
+
+CREATE database job_list;
+
+USE job_list;
+
+CREATE TABLE department_id (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE role_id (
+  id INT NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL,
+  department_id INT,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE employee_id (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT,
+  manager_id INT
+
+  PRIMARY KEY (id)
+);
---
+++ server.js
@@ -0,0 +1,36 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "rootroot",
+  database: "top_songsDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runSearch();
+});
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Find songs by artist",
+        "Find all artists who appear more than once",
+        "Find data within a specific range",
+        "Search for a specific song",
+        "exit"
+      ]
+    })}
