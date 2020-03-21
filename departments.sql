8f3b3ff57dd07895705c254789e921c327e139a3
Leticia Guerra <g.leticia@rocketmail.com>
2020-03-21 17:42:32 -0400

    updated schema

--- departments.sql
+++ departments.sql
@@ -1,50 +1,27 @@
--- * **department**:
-
---   * **id** - INT PRIMARY KEY
---   * **name** - VARCHAR(30) to hold department name
-
--- * **role**:
-
---   * **id** - INT PRIMARY KEY
---   * **title** -  VARCHAR(30) to hold role title
---   * **salary** -  DECIMAL to hold role salary
---   * **department_id** -  INT to hold reference to department role belongs to
-
--- * **employee**:
-
---   * **id** - INT PRIMARY KEY
---   * **first_name** - VARCHAR(30) to hold employee first name
---   * **last_name** - VARCHAR(30) to hold employee last name
---   * **role_id** - INT to hold reference to role employee has
---   * **manager_id** - INT to hold reference to another employee that manager of the current employee. This field may be null if the employee has no manager
-
+DROP DATABASE IF EXISTS job_list;
 CREATE database job_list;
 
-USE job_list;
-
-CREATE TABLE department_id (
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE role_id (
-  id INT NOT NULL,
-  title VARCHAR(30) NOT NULL,
-  salary DECIMAL,
-  department_id INT,
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE employee_id (
-  id INT NOT NULL,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT,
-  manager_id INT,
-
-  PRIMARY KEY (id)
-);
-SELECT * FROM job_list.department_id;
-SELECT * FROM job_list.employee_id;
-SELECT * FROM job_list.role_id;
+-- USE job_list;
+-- CREATE TABLE `employees_id` (
+--   `id` int DEFAULT NULL,
+--   `first_names` text,
+--   `last_name` text,
+--   `role_id` text,
+--   `department_id` text,
+--   `salary_id` int DEFAULT NULL,
+--   `manager_id` text
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- SELECT * FROM job_list.employees_id;
+-- USE job_list;
+-- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+-- VALUES (1,"Wreck-it","Ralph", "Wrecks Things","Retro-Game's",10,"Fix-it Felix");
+-- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+-- VALUES (2,"Vanelope","von Schweetz", "Racecar Driver","Sugar Rush",50000,"Wreck-it Ralph");
+-- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+-- VALUES (3,"Fix-it","Felix", "Fixes Things","Retro-Games",100,"Disney");
+-- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+-- VALUES (4,"Sergeant","Calhoun", "Cybug-Hunter","Hero's Duty",3000,"Stan Litwak");
+-- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+-- VALUES (5,"King","Candy","King","Sugar Rush",10000,"Turbo");
+-- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+-- VALUES (6,"Taffyta","Muttonfudge","Racecar Driver","Sugar Rush",30000,"Vanelope von Schweetz");
