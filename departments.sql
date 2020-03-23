dc272488a91622cf162e083b328f63f22f952148
Leticia Guerra <g.leticia@rocketmail.com>
2020-03-23 18:06:07 -0400

    new

--- departments.sql
+++ departments.sql
@@ -1,27 +1,27 @@
 DROP DATABASE IF EXISTS job_list;
 CREATE database job_list;
 
--- USE job_list;
--- CREATE TABLE `employees_id` (
---   `id` int DEFAULT NULL,
---   `first_names` text,
---   `last_name` text,
---   `role_id` text,
---   `department_id` text,
---   `salary_id` int DEFAULT NULL,
---   `manager_id` text
--- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- SELECT * FROM job_list.employees_id;
--- USE job_list;
--- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
--- VALUES (1,"Wreck-it","Ralph", "Wrecks Things","Retro-Game's",10,"Fix-it Felix");
--- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
--- VALUES (2,"Vanelope","von Schweetz", "Racecar Driver","Sugar Rush",50000,"Wreck-it Ralph");
--- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
--- VALUES (3,"Fix-it","Felix", "Fixes Things","Retro-Games",100,"Disney");
--- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
--- VALUES (4,"Sergeant","Calhoun", "Cybug-Hunter","Hero's Duty",3000,"Stan Litwak");
--- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
--- VALUES (5,"King","Candy","King","Sugar Rush",10000,"Turbo");
--- INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
--- VALUES (6,"Taffyta","Muttonfudge","Racecar Driver","Sugar Rush",30000,"Vanelope von Schweetz");
+USE job_list;
+CREATE TABLE `employees_id` (
+  `id` int DEFAULT NULL,
+  `first_names` text,
+  `last_name` text,
+  `role_id` text,
+  `department_id` text,
+  `salary_id` int DEFAULT NULL,
+  `manager_id` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+SELECT * FROM job_list.employees_id;
+USE job_list;
+INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+VALUES (1,"Wreck-it","Ralph", "Wrecks Things","Retro-Game's",10,"Fix-it Felix");
+INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+VALUES (2,"Vanelope","von Schweetz", "Racecar Driver","Sugar Rush",50000,"Wreck-it Ralph");
+INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+VALUES (3,"Fix-it","Felix", "Fixes Things","Retro-Games",100,"Disney");
+INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+VALUES (4,"Sergeant","Calhoun", "Cybug-Hunter","Hero's Duty",3000,"Stan Litwak");
+INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+VALUES (5,"King","Candy","King","Sugar Rush",10000,"Turbo");
+INSERT INTO employees_id (id, first_names, last_name, role_id, department_id, salary_id, manager_id)
+VALUES (6,"Taffyta","Muttonfudge","Racecar Driver","Sugar Rush",30000,"Vanelope von Schweetz");
